45e108992ac54b461ec48a4e71273ebd461d3c7e
刘志远 <80236363+Ga1axyz@users.noreply.github.com>
2021-06-11 19:11:10 +0800

    增加私聊功能；优化代码；

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@
  * @Author: Ga1axy_z
  * @Date: 2021-05-19 21:30:46
  * @LastEditors: Ga1axy_z
- * @LastEditTime: 2021-06-10 16:59:19
+ * @LastEditTime: 2021-06-11 15:21:01
  */
 
 // 客户端开启连接
@@ -17,6 +17,17 @@ var username, password, sex, avatar;
 // 5.20
 // 当用户传入一张过大的图片，会导致图片发送失败，且用户会被踢下线
 
+// 此变量用来辅助在聊天界面按下 Ctrl+Enter 键发送消息的功能，防止在其他页面按下此组合键触发响应动作
+var ctrl_enter = 0;
+// 此变量用来辅助在登录界面按下 Enter 键登录
+var login_enter = 1;
+// 此变量用来辅助在注册界面按下 Enter 键注册
+var register_enter = 0;
+// 此变量用来辅助存储聊天消息
+var private_chat = 0;
+var receive_user = '';
+var send_user = '';
+var userNum = 0;
 
 // ****************** 登录功能 ******************
 
@@ -38,8 +49,25 @@ $('#loginBtn').on('click', () => {
     })
 })
 
-// 此变量用来辅助在聊天界面按下 Ctrl+Enter 键发送消息的功能，防止在其他页面按下此组合键触发响应动作
-var ctrl_enter = 0;
+// 实现登录界面 Enter 键登录
+window.addEventListener('keydown', e => {
+    if(e.key == 'Enter' && login_enter == 1){
+        username = $("#username").val().trim();  // jQuery val()方法返回或设置被选元素的 value 属性
+        password = $('#password').val().trim();
+        if (!username || !password) {
+            alert('用户名或密码为空，\n请正确填写！');
+            return;
+        }
+        console.log(username, password);
+
+        // 将用户名和密码提交给服务器，校验用户身份
+        // 服务器端和客户端之间可以使用 socket 端口对象的 emit() 方法，互相发送事件 socket.emit(event,data,[callback])
+        socket.emit('checkoutLogin', {
+            username: username,
+            password: password
+        })
+    }
+})
 
 // 监听用户身份校验结果
 // 一方使用 emit() 发送事件后，另一方可以使用 on() 或者 once() 方法，对该事件进行监听 socket.on(event,function(data,fn){})
@@ -63,7 +91,10 @@ socket.on('loginSuccess', data => {
     // 跳转到聊天室
     $('.login_box').fadeOut();   // 隐藏登录窗口 淡出效果
     $('.container').fadeIn();    // 显示聊天窗口 淡入效果
+    alert("欢迎来到 Ga1axy_z 的聊天室!\n\n默认为公共聊天室模式，\n点击左侧用户列表即可进入私聊。\n点击右上角返回按钮即可返回公共聊天。\n\nHave A Nice Day!\n");
     ctrl_enter = 1;
+    login_enter = 0;
+    register_enter = 0;
     // 设置个人信息，用于在聊天窗口中标识当前用户身份
     $('.avatar_url').attr('src', data.avatar);   // jQuery attr()方法设置或返回被选元素的属性和值
     $('.user-list .username').text(data.username);   // jQuery text()方法设置或返回被选元素的文本内容
@@ -83,7 +114,7 @@ socket.on('addUser', data => {
     $('.box-bd').append(`
         <div class="system">
             <p class="message_system">
-                <span class="content">"${data.username}"加入了群聊</span>
+                <span class="content">"${data.username}"加入了聊天</span>
             </p>
         </div>
     `);
@@ -94,7 +125,7 @@ socket.on('deleteUser', data => {
     $('.box-bd').append(`
         <div class="system">
             <p class="message_system">
-                <span class="content">"${data.username}"离开了群聊</span>
+                <span class="content">"${data.username}"离开了聊天</span>
             </p>
         </div>
     `);
@@ -106,15 +137,17 @@ socket.on('userList', data => {
     // 更新列表前先将其清空
     $('.user-list ul').html('');
     data.forEach(item => {
+        //  id="${item.username}" 用来辅助实现点击用户列表中的用户获取用户名
         $('.user-list ul').append(`
-            <li class="user">
-                <div class="avatar"><img src="${item.avatar}" alt="" /></div>
-                <div class="name">${item.username}</div>
-            </li> 
+            <li class="user" id="${item.username}">
+                <div class="avatar"  id="${item.username}"><img src="${item.avatar}" id="${item.username}" alt="" /></div>
+                <div class="name"  id="${item.username}">${item.username}</div>
+            </li>
         `)
     });
     // 更新在线用户数量
     $('#userCount').text(data.length);
+    userNum = data.length;
 })
 
 // 实现 Ctrl+Enter 发送消息
@@ -128,15 +161,31 @@ window.addEventListener('keydown', e => {
             return alert('发送内容不能为空！');
         if (content.indexOf("进平") >= 0)   // indexOf()方法对大小写敏感，如果要检索的字符串值没有出现，则该方法返回 -1
             return alert('别闹！再斟酌斟酌用词！');
-        let message = {
-            content: content,
-            username: username,
-            avatar: avatar,
-            type: 'html'
+        if (private_chat == 0) {
+            let message = {
+                content: content,
+                username: username,
+                avatar: avatar,
+                type: 'html',
+                receive: 'all',
+                send: username
+            }
+            // 将相关信息发送给服务器
+            socket.emit('sendMessage', message);
+            console.log(message);
+        }else {
+            let message = {
+                content: content,
+                username: username,
+                avatar: avatar,
+                type: 'html',
+                receive: receive_user,
+                send: username
+            }
+            // 将相关信息发送给服务器
+            socket.emit('sendMessage', message);
+            console.log(message);
         }
-        // 将相关信息发送给服务器
-        socket.emit('sendMessage', message);
-        console.log(message);
     }
 })
 
@@ -160,15 +209,31 @@ $('#btn-send').on('click', () => {
         return alert('发送内容不能为空！');
     if (content.indexOf("进平") >= 0)   // indexOf()方法对大小写敏感，如果要检索的字符串值没有出现，则该方法返回 -1
         return alert('别闹！再斟酌斟酌用词！');
-    let message = {
-        content: content,
-        username: username,
-        avatar: avatar,
-        type: 'html'
+    if (private_chat == 0) {
+        let message = {
+            content: content,
+            username: username,
+            avatar: avatar,
+            type: 'html',
+            receive: 'all',
+            send: username
+        }
+        // 将相关信息发送给服务器
+        socket.emit('sendMessage', message);
+        console.log(message);
+    }else {
+        let message = {
+            content: content,
+            username: username,
+            avatar: avatar,
+            type: 'html',
+            receive: receive_user,
+            send: username
+        }
+        // 将相关信息发送给服务器
+        socket.emit('sendMessage', message);
+        console.log(message);
     }
-    // 将相关信息发送给服务器
-    socket.emit('sendMessage', message);
-    console.log(message);
 })
 // 实时更新文本类型的聊天记录
 socket.on('receiveMessage', data => {
@@ -216,12 +281,25 @@ $('#file').on('change', function () {
     var fr = new FileReader();  // FileReader 对象允许 Web 应用程序异步读取存储在用户计算机上的文件
     fr.readAsDataURL(file);
     fr.onload = () => {
-        socket.emit('sendImage', {
-            username: username,
-            avatar: avatar,
-            img: fr.result,
-            type: 'image'
-        })
+        if (private_chat == 0) {
+            socket.emit('sendImage', {
+                username: username,
+                avatar: avatar,
+                img: fr.result,
+                type: 'image',
+                receive: 'all',
+                send: username
+            })
+        } else {
+            socket.emit('sendImage', {
+                username: username,
+                avatar: avatar,
+                img: fr.result,
+                type: 'image',
+                receive: receive_user,
+                send: username
+            })
+        }
     }
 })
 // 实时更新图片类型的聊天记录
@@ -417,11 +495,17 @@ $('#register_avatar li').on('click', function () {
 $('#registerBtn').on('click', () => {
     $('.login_box').fadeOut();   // 隐藏登陆窗口 淡出效果
     $('.register_box').fadeIn(); // 显示注册窗口 淡入效果
+    ctrl_enter = 0;
+    login_enter = 0;
+    register_enter = 1;
 })
 // 返回登录页面
 $('#returnBtn').on('click', () => {
     $('.register_box').fadeOut();   // 隐藏注册窗口 淡出效果
     $('.login_box').fadeIn();       // 显示登录窗口 淡入效果
+    ctrl_enter = 0;
+    login_enter = 1;
+    register_enter = 0;
 })
 // 点击注册按钮
 $('#register').on('click', () => {
@@ -442,6 +526,27 @@ $('#register').on('click', () => {
         avatar: avatar
     })
 })
+// 实现注册界面 Enter 键注册
+window.addEventListener('keydown', e => {
+    if(e.key == 'Enter' && register_enter == 1){
+        username = $('#register_username').val().trim();
+        password = $('#register_password').val().trim();
+        sex = $('#sex input[name=sex]:checked').val();
+        avatar = $('#register_avatar li.now img').attr('src');
+        console.log(username, password, sex, avatar);
+        if (!username || !password || !sex || !avatar) {
+            alert('请将信息填写完整后再提交！');
+            return;
+        }
+        // 将用户信息提交给服务器
+        socket.emit('registerUser', {
+            username: username,
+            password: password,
+            sex: sex,
+            avatar: avatar
+        })
+    }
+})
 // 监听注册成功的请求
 socket.on('registerSuccess', function () {
     alert('注册成功！\n点击登录按钮即可进入聊天室')
@@ -456,4 +561,32 @@ socket.on('registerSuccess', function () {
 // 监听注册失败的请求
 socket.on('registerError', function () {
     alert('此用户名已被注册！\n请更换你的昵称！');
+})
+
+// ****************** 私聊功能 ******************
+
+// 监听点击左侧用户列表
+$(document).on('click', ".user-ul li", (e) => {
+    console.log(e.target.id);
+    receive_user = e.target.id;
+    send_user = username;
+    private_chat = 1;
+    $('.backBtn').fadeIn();       // 显示返回按钮 淡入效果
+    $('.box-bd').html('');
+    $('.box-hd').html('');
+    $('.box-hd').html('<h3>'+ receive_user +'</h3>');
+    socket.emit('privateChat', {
+        receive_user: receive_user,
+        send_user: send_user
+    })
+})
+// 监听点击返回公共聊天按钮
+$('.backBtn').on('click', () => {
+    private_chat = 0;
+    $('.backBtn').fadeOut();   // 隐藏返回按钮 淡出效果
+    $('.box-bd').html('');
+    $('.box-hd').html('');
+    $('.box-hd').html("<h3>Ga1axy_z's WebMessage (<span id='userCount'>99+</span>)</h3>");
+    $('#userCount').text(userNum);
+    socket.emit('publicChat', {})
 })
